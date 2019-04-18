45fc2fb22a4445dca0685114994fe3ce745c0757
Yonny Sanchez <yonnylsb@users.noreply.github.com>
2019-04-18 09:03:55 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        disableHostCheck: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin()
+    ]
+}
